39bedb4aa14fecf3545bce30ba58e25c46b2c4d9
Miguel <miguel.040.net@gmail.com>
2026-03-19 23:12:07 -0400

    mejorando ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,9 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Sentri',
+  description: 'Plataforma de seguimiento académico',
+  generator: 'Sentri',
   icons: {
     icon: [
       {
